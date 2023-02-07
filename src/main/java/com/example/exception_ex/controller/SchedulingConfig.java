991607353b801bf20803bec3208e40be8c36016f
Hassan Refaat <hassan.refaat.dev@gmail.com>
2023-02-07 11:49:41 +0200

    @Aysnc added

--- src/main/java/com/example/exception_ex/controller/SchedulingConfig.java
+++ src/main/java/com/example/exception_ex/controller/SchedulingConfig.java
@@ -2,6 +2,8 @@ package com.example.exception_ex.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -10,10 +12,12 @@ import java.time.LocalTime;
 
 @Component
 @EnableScheduling
+@EnableAsync
 public class SchedulingConfig {
     private final Logger logger = LoggerFactory.getLogger(SchedulingConfig.class);
 
-    @Scheduled(fixedRate = 1000)
+    @Scheduled(fixedRate = 2000)
+    @Async
     public void printTimeInLog() throws InterruptedException {
         Thread.sleep(4000);
         logger.info("The current time is {}", LocalTime.now());
